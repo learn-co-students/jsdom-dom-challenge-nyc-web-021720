f4bd29d85ce08d7f36effa2a198ddd2934eab9cc
jguzz <joseph.a.guzzardo@gmail.com>
2020-04-07 10:40:25 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -2,8 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("the dom has loaded");
   // Counter
   const counter = document.querySelector("h1#counter");
+  const minus = document.querySelector("button#minus");
+  const plus = document.querySelector("button#plus");
+  const like = document.querySelector("button#heart");
+  const likesUl = document.querySelector("ul.likes");
+
+  //Adds 1 to the counter every second
   setInterval(() => {
-    //   Adds 1 to the counter every second
     counter.innerText = parseInt(counter.innerText) + 1;
   }, 1000);
+
+  //Adds 1 to the counter
+  plus.addEventListener("click", () => {
+    counter.innerText = parseInt(counter.innerText) + 1;
+  });
+
+  //Subtracts 1 from the counter
+  minus.addEventListener("click", () => {
+    counter.innerText = parseInt(counter.innerText) - 1;
+  });
+
+  like.addEventListener("click", () => {
+    const newLike = document.createElement("li");
+    newLike.innerText = counter.innerText;
+    likesUl.appendChild(newLike);
+  });
 });
